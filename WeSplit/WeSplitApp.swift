da134ac5b42e373e9dd579041e356c87c008f22d
Mattia Fasoli <mattia.fasoli@socialthingum.com>
2021-04-29 13:01:40 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mattia Fasoli on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
